cb5875a08808382c9a86931e14c77c81fd672649
syskn <bit192@gmail.com>
2023-03-19 18:12:13 +0900

    test

--- src/transformers/models/gpt_neox/__init__.py
+++ src/transformers/models/gpt_neox/__init__.py
@@ -2,7 +2,7 @@
 # There's no way to ignore "F401 '...' imported but unused" warnings in this
 # module, but to preserve other warnings. So, don't check this module at all.
 
-# Copyright 2021 The EleutherAI and HuggingFace Teams. All rights reserved.
+# Copyright 2022 The HuggingFace Team. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -17,46 +17,60 @@
 # limitations under the License.
 from typing import TYPE_CHECKING
 
-from ...file_utils import _LazyModule, is_flax_available, is_torch_available
+from ...file_utils import _LazyModule, is_tokenizers_available, is_torch_available
+from ...utils import OptionalDependencyNotAvailable
 
 
-_import_structure = {
-    "configuration_GPTNeoX": ["GPTNeoX_PRETRAINED_CONFIG_ARCHIVE_MAP", "GPTNeoXConfig"],
-}
+_import_structure = {"configuration_gpt_neox": ["GPT_NEOX_PRETRAINED_CONFIG_ARCHIVE_MAP", "GPTNeoXConfig"]}
 
-if is_torch_available():
-    _import_structure["modeling_GPTNeoX"] = [
-        "GPTNeoX_PRETRAINED_MODEL_ARCHIVE_LIST",
+try:
+    if not is_tokenizers_available():
+        raise OptionalDependencyNotAvailable()
+except OptionalDependencyNotAvailable:
+    pass
+else:
+    _import_structure["tokenization_gpt_neox_fast"] = ["GPTNeoXTokenizerFast"]
+
+try:
+    if not is_torch_available():
+        raise OptionalDependencyNotAvailable()
+except OptionalDependencyNotAvailable:
+    pass
+else:
+    _import_structure["modeling_gpt_neox"] = [
+        "GPT_NEOX_PRETRAINED_MODEL_ARCHIVE_LIST",
         "GPTNeoXForCausalLM",
-        "GPTNeoXForQuestionAnswering",
-        "GPTNeoXForSequenceClassification",
+        "GPTNeoXLayer",
         "GPTNeoXModel",
         "GPTNeoXPreTrainedModel",
     ]
 
-if is_flax_available():
-    _import_structure["modeling_flax_GPTNeoX"] = [
-        "FlaxGPTNeoXForCausalLM",
-        "FlaxGPTNeoXModel",
-        "FlaxGPTNeoXPreTrainedModel",
-    ]
-
 
 if TYPE_CHECKING:
-    from .configuration_GPTNeoX import GPTNeoX_PRETRAINED_CONFIG_ARCHIVE_MAP, GPTNeoXConfig
+    from .configuration_gpt_neox import GPT_NEOX_PRETRAINED_CONFIG_ARCHIVE_MAP, GPTNeoXConfig
 
-    if is_torch_available():
-        from .modeling_GPTNeoX import (
-            GPTNeoX_PRETRAINED_MODEL_ARCHIVE_LIST,
+    try:
+        if not is_tokenizers_available():
+            raise OptionalDependencyNotAvailable()
+    except OptionalDependencyNotAvailable:
+        pass
+    else:
+        from .tokenization_gpt_neox_fast import GPTNeoXTokenizerFast
+
+    try:
+        if not is_torch_available():
+            raise OptionalDependencyNotAvailable()
+    except OptionalDependencyNotAvailable:
+        pass
+    else:
+        from .modeling_gpt_neox import (
+            GPT_NEOX_PRETRAINED_MODEL_ARCHIVE_LIST,
             GPTNeoXForCausalLM,
-            GPTNeoXForQuestionAnswering,
-            GPTNeoXForSequenceClassification,
+            GPTNeoXLayer,
             GPTNeoXModel,
             GPTNeoXPreTrainedModel,
         )
 
-    if is_flax_available():
-        from .modeling_flax_GPTNeoX import FlaxGPTNeoXForCausalLM, FlaxGPTNeoXModel, FlaxGPTNeoXPreTrainedModel
 
 else:
     import sys
