92766c27d529c496be597be99437cb4ede0c14cc
syskn <bit192@gmail.com>
2023-05-19 19:42:06 +0900

    Update modeling_gpt_neox.py

--- src/transformers/models/gpt_neox/modeling_gpt_neox.py
+++ src/transformers/models/gpt_neox/modeling_gpt_neox.py
@@ -565,7 +565,7 @@ class GPTNeoXModel(GPTNeoXPreTrainedModel):
             # Set threshold with model.gpt_neox.set_early_exit_entropy()
             # Threshold of 0.01 is recommended
             if not self.training and self.early_exit_entropy >= 0:
-                if i >= 16 and i < self.config.num_hidden_layers:
+                if i >= 12 and i % 4 == 0 and i < self.config.num_hidden_layers:
                     try:
                         highway_logit = self.final_layer_norm(hidden_states).squeeze(0)
                         highway_entropy = (highway_logit[0].max() - highway_logit[0].mean()).item()
